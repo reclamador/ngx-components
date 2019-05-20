01c5dc155c0462c5609ca612b8a0a2ecb0e295f8
David Ruiz López <ruizfrontend@gmail.com>
2019-05-20 16:48:16 +0200

    first scafooldings

---
+++ libs/reclamitux/src/lib/reclamitux-showcase/reclamitux-showcase.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-reclamitux-showcase',
+  templateUrl: './reclamitux-showcase.component.html',
+  styleUrls: ['./reclamitux-showcase.component.scss']
+})
+export class ReclamituxShowcaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ libs/reclamitux/src/lib/reclamitux.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReclamituxShowcaseComponent } from './reclamitux-showcase/reclamitux-showcase.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ReclamituxShowcaseComponent]
+})
+export class ReclamituxModule {}
