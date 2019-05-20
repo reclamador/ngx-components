acd80984bd8b01169924bf290e5d54ea54d9036d
David Ruiz López <ruizfrontend@gmail.com>
2019-05-20 18:02:26 +0200

    first component published

--- apps/demo-page/src/app/app.module.ts
+++ apps/demo-page/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ReclamituxModule } from './../../../../libs/reclamitux/src/lib/reclamitux.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ReclamituxModule],
+  imports: [BrowserModule, ReclamituxModule, BrowserAnimationsModule, NbThemeModule.forRoot({ name: 'default' }), NbLayoutModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
